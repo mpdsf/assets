b47d4d7cf88fb73047e12b84e6c7f98ec109af8d
Dave Schweisguth <dave@schweisguth.org>
2021-08-30 08:54:02 -0700

    Update version to 0.9

---
+++ google-analytics-config.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-3MNS5PY9VX', {
+    cookie_domain: 'mpdsf.github.io',
+    cookie_flags: 'SameSite=None;Secure'
+});
